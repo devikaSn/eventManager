9bde954ef79fa25a529822f986a74e21135e173e
Devika S <devika@qburst.com>
2017-08-09 13:02:14 +0530

    Added selection and deselection of calendar event cells

--- wordpress/wp-content/themes/eventtheme/main.js
+++ wordpress/wp-content/themes/eventtheme/main.js
@@ -1,9 +1,31 @@
 
-/* Selection and deselction of event cells*/
 
 $(document).ready(function(){
+
+	 /* Selection and deselction of weekday event cells*/
 	$('table.cal_blue.ecwd_calendar_container .has-events').click(function(e){
-	   $('table.cal_blue.ecwd_calendar_container .has-events').removeClass('active');
-       $(this).addClass('active');
+
+	   $('.cal_blue.ecwd_calendar_container .events li > a').removeClass('active');
+	   $('.cal_blue.ecwd_calendar_container .events li > span').removeClass('active');
+	   $('table.cal_blue.ecwd_calendar_container .no-events').removeClass('active');
+	   
+	   // $(this).find(".ecwd_theme_calendar .ecwd_calendar td .day-number").addClass('active');
+	   $('.cal_blue.ecwd_calendar_container .has-events').removeClass('active'); 
+       $(this).addClass('active'); //Adding selection color to cell
+       $(this).find("li > a").addClass('active'); //Adding selection color to cell elements
+       $(this).find("li > span").addClass('active'); //Adding selection color to more events link
+
 	});
+
+	/* Selection and deselction of weekend event cells*/
+	$('table.cal_blue.ecwd_calendar_container .no-events').click(function(){
+
+		$('.cal_blue.ecwd_calendar_container .has-events').removeClass('active'); 
+	    $('.cal_blue.ecwd_calendar_container .events li > a').removeClass('active');
+	    $('.cal_blue.ecwd_calendar_container .events li > span').removeClass('active');
+
+		$('table.cal_blue.ecwd_calendar_container .no-events').removeClass('active');
+		$(this).addClass('active');
+	});
+
 });
